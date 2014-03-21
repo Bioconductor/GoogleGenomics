a25b513695b17baeb6754af9d3bec1be4686937c
cassiedoll <cassandradoll@gmail.com>
2014-03-21 10:04:45 -0700

    Add a simple r client

---
+++ README.md
@@ -0,0 +1,43 @@
+client-r
+==============
+
+This R client fetches data from the
+<a href="https://developers.google.com/genomics">Google Genomics API</a> and
+turns it into a GAlignments object provided by the
+<a href="http://master.bioconductor.org/packages/release/bioc/html/GenomicRanges.html">GenomicRanges package.</a>
+
+This GAlignments object is then plotted using
+<a href="http://master.bioconductor.org/packages/release/bioc/html/ggbio.html">ggbio</a>
+- but it can also be integrated with any of the other R packages that support GAlignments or GRanges.
+
+###Getting started
+
+* First you'll need to setup an <a href="http://www.r-project.org/">R environment</a>.
+
+* Then you'll need a valid client ID and secret. Follow the
+<a href="https://developers.google.com/genomics#authenticate">authentication instructions</a>,
+but instead of downloading the json file, you'll pass the 'Client ID' and
+'Client secret' values into the setup function.
+
+* In an R interpreter:
+```
+source("/path/to/genomics-tools/client-r/genomics-api.R")
+setup("<client ID>", "<client secret>")
+getReadData()
+```
+
+* The setup function only needs to be run once. After it has been called, the
+getReadData function can then be run repeatedly. It gets data from the API
+and can be used to search over any set of reads. You can pull up a different
+sequence position my specifying additional arguments:
+```
+getReadData(chromosome="chr3", start="121459049", end="124")
+```
+
+Or, you can use the readsetId argument to query a different readset entirely:
+```
+getReadData(readsetId="<myreadset>")
+```
+
+Both `reads` and `alignments` are exported as global variables so that you
+can use other bioconductor tools to modify the data as you wish.
---
+++ genomics-api.R
@@ -0,0 +1,78 @@
+# Copyright 2014 Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This is a "client ID for native application" id and secret from the Google Developer's console
+# (console.developers.google.com). Make sure you pass in your own values.
+setup <- function(clientKey="...googleusercontent.com", clientSecret) {
+  install.packages("rjson")
+  install.packages("devtools")
+  devtools::install_github("hadley/httr") # We need the dev version of httr for now
+    
+  # Bioconductor packages
+  source("http://bioconductor.org/biocLite.R")
+  biocLite("GenomicRanges")
+  biocLite("ggbio")
+  biocLite("Rsamtools")
+
+  library(rjson)
+  library(httr)
+  library(ggbio)
+  library(Rsamtools)
+
+  app <- oauth_app("google", clientKey, clientSecret)
+  google_token <<- oauth2.0_token(oauth_endpoints("google"), app,
+      scope = "https://www.googleapis.com/auth/genomics")
+}
+
+# By default, this function encompasses 2 chromosome positions which relate to ApoE
+# (http://www.snpedia.com/index.php/Rs429358 and http://www.snpedia.com/index.php/Rs7412)
+getReadData <- function(chromosome = "chr19", start = 45411941, end = 45412079,
+    readsetId = "CJ_ppJ-WCxDxrtDr5fGIhBA=", endpoint="https://www.googleapis.com/genomics/v1beta/") {
+    	
+  # Fetch data from the Genomics API  	
+  body <- list(readsetIds=list(readsetId), sequenceName=chromosome, sequenceStart=start, sequenceEnd=end)  	
+    	
+  res <- POST(paste(endpoint, "reads/search", sep=""),
+    query=list(fields="reads(name,cigar,position,originalBases,flags)"),
+    body=toJSON(body), config(token=google_token), add_headers("Content-Type"="application/json"))
+  
+  stop_for_status(res)
+  reads <<- content(res)$reads
+
+  # Transform the Genomics API data into a GAlignments object
+  names = sapply(reads, '[[', 'name')
+  cigars = sapply(reads, '[[', 'cigar')
+  positions = as.integer(sapply(reads, '[[', 'position'))
+  flags = sapply(reads, '[[', 'flags')
+  
+  isMinusStrand = bamFlagAsBitMatrix(as.integer(flags), bitnames="isMinusStrand")
+  total_reads = length(positions)
+
+  alignments <<- GAlignments(
+      seqnames=Rle(c(chromosome), c(total_reads)), 
+      strand=strand(as.vector(ifelse(isMinusStrand, '-', '+'))), 
+      pos=positions, cigar=cigars, names=names, flag=flags)
+ 
+  plotAlignments()    
+}
+
+plotAlignments <- function() {    
+  # Display the basic alignments    
+  p1 <- autoplot(alignments, color="gray40", fill="skyblue") 
+  p1
+
+  # You could also display the spot on the chromosome these alignments came from     
+  # p2 <- plotIdeogram(genome="hg19", subchr=chromosome)  
+  # p2 + xlim(as(alignments, 'GRanges'))
+}    
