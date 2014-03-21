2c7d566f84f3f38216e0f7b82f1b8454bc96062f
cassiedoll <cassandradoll@gmail.com>
2014-03-21 14:20:12 -0700

    Add pagination support, improve default graphs
    The default graph now shows strand and coverage data, and also labels
    the x axis.

--- genomics-api.R
+++ genomics-api.R
@@ -38,17 +38,22 @@ setup <- function(clientId="...googleusercontent.com", clientSecret) {
 # By default, this function encompasses 2 chromosome positions which relate to ApoE
 # (http://www.snpedia.com/index.php/Rs429358 and http://www.snpedia.com/index.php/Rs7412)
 getReadData <- function(chromosome="chr19", start=45411941, end=45412079,
-    readsetId="CJ_ppJ-WCxDxrtDr5fGIhBA=", endpoint="https://www.googleapis.com/genomics/v1beta/") {
+    readsetId="CJ_ppJ-WCxDxrtDr5fGIhBA=", endpoint="https://www.googleapis.com/genomics/v1beta/", 
+    pageToken=NULL) {
     	
   # Fetch data from the Genomics API  	
-  body <- list(readsetIds=list(readsetId), sequenceName=chromosome, sequenceStart=start, sequenceEnd=end)  	
+  body <- list(readsetIds=list(readsetId), sequenceName=chromosome, sequenceStart=start, sequenceEnd=end, pageToken=pageToken)  	
+    	
+  message("Fetching read data")
     	
   res <- POST(paste(endpoint, "reads/search", sep=""),
-    query=list(fields="reads(name,cigar,position,originalBases,flags)"),
-    body=toJSON(body), config(token=google_token), add_headers("Content-Type"="application/json"))
-  
+    query=list(fields="nextPageToken,reads(name,cigar,position,originalBases,flags)"),
+    body=toJSON(body), config(token=google_token), add_headers("Content-Type"="application/json"))  
   stop_for_status(res)
-  reads <<- content(res)$reads
+  
+  message("Parsing read data")
+  res_content <- content(res)
+  reads <<- res_content$reads
 
   # Transform the Genomics API data into a GAlignments object
   names = sapply(reads, '[[', 'name')
@@ -58,24 +63,36 @@ getReadData <- function(chromosome="chr19", start=45411941, end=45412079,
   
   isMinusStrand = bamFlagAsBitMatrix(as.integer(flags), bitnames="isMinusStrand")
   total_reads = length(positions)
+  
+  if (is.null(pageToken)) {
+  	# If this is the first getReadData request, clear out any existing alignments
+  	# Otherwise we will append our data to what we've retrieved before
+  	alignments <<- NULL
+  }
 
-  alignments <<- GAlignments(
+  alignments <<- c(GAlignments(
       seqnames=Rle(c(chromosome), c(total_reads)), 
       strand=strand(as.vector(ifelse(isMinusStrand, '-', '+'))), 
-      pos=positions, cigar=cigars, names=names, flag=flags)
- 
-  plotAlignments()    
+      pos=positions, cigar=cigars, names=names, flag=flags), alignments)      
+  
+  if (!is.null(res_content$nextPageToken)) {
+ 	message(paste("Continuing read query with the nextPageToken:", res_content$nextPageToken))  	
+  	getReadData(chromosome=chromosome, start=start, end=end, readsetId=readsetId, 
+  	    endpoint=endpoint, pageToken=res_content$nextPageToken)
+  } else {
+    plotAlignments(xlab=chromosome)   
+  }
 }
 
-plotAlignments <- function() {    
+plotAlignments <- function(xlab="") {      
   # Display the basic alignments    
-  p1 <- autoplot(alignments, color="gray40", fill="skyblue") 
-  p1
+  p1 <- autoplot(alignments, aes(color=strand, fill=strand))   
+  
+  # And coverage data
+  p2 <- ggplot(as(alignments, 'GRanges')) + stat_coverage(color="gray40", fill="skyblue")
+  tracks(p1, p2, xlab=xlab)
 
   # You could also display the spot on the chromosome these alignments came from     
-  # p2 <- plotIdeogram(genome="hg19", subchr=chromosome)  
-  # p2 + xlim(as(alignments, 'GRanges'))
-  
-  # You could also plot the coverage
-  # ggplot(as(alignments, 'GRanges')) + stat_coverage()
+  # p3 <- plotIdeogram(genome="hg19", subchr=chromosome)  
+  # p3 + xlim(as(alignments, 'GRanges'))
 }    
