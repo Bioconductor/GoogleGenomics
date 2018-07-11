70f51a223accbadea25f0d406f6c9675e6e113de
lshep <shepherl@gmail.com>
2018-07-11 03:39:37 -0400

    User requested deprecation
      - https://support.bioconductor.org/p/110861/

--- R/zzz.R
+++ R/zzz.R
@@ -25,3 +25,10 @@
 
   invisible()
 }
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.8")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
