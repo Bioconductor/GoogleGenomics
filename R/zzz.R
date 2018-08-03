f58dcd324fe3d6e08454fced3c2e92b194cd3db2
lshep <shepherl@gmail.com>
2018-08-03 09:09:20 -0400

    Update package deprecation message

--- R/zzz.R
+++ R/zzz.R
@@ -29,6 +29,6 @@
 .onAttach <- function(libname, pkgname) {
     msg <- sprintf(
         "Package '%s' is deprecated and will be removed from Bioconductor
-         version %s", pkgname, "3.8")
+         version %s", pkgname, "3.9")
     .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
 }
